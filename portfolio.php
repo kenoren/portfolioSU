6016cf0ad819c4d0db9c19df85db9a2672333763
Mathis Fourey <kenoren@users.noreply.github.com>
2024-11-03 12:17:46 +0100

    page accueil commencement

---
+++ portfolio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/portfolio.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <title>Portfolio - Mathis.F</title>
+</head>
+<body>
+    <div class="circle circle1"></div>
+    <div class="circle circle2"></div>
+    <div class="container">
+        <header>
+            <img src="./assets/images/logo.png" alt="logo">
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Compétences</li>
+                    <li>Réalisation</li>
+                    <li>Formation</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>  
+        </header>
+
+        <main>
+            <img src="./assets/images/moi.png" alt="moi">
+        </main>
+    </div>
+</body>
+</html>
